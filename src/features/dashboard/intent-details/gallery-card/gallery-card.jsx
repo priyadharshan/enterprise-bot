0cbf58ac860db52f41e0d797ddcc8649886501c1
priyadharshan <priyadharshan91@gmail.com>
2020-06-12 05:24:48 +0530

    completed

--- src/features/dashboard/intent-details/gallery-card/gallery-card.jsx
+++ src/features/dashboard/intent-details/gallery-card/gallery-card.jsx
@@ -1,7 +1,24 @@
 import React, { memo } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 170,
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 
 export const GalleryCard = memo(() => {
+    const classes = useStyles();
 
   return (
       <div className="suggestion-block">
@@ -13,8 +30,29 @@ export const GalleryCard = memo(() => {
           <i class="material-icons delete-outline-icon">delete_outline</i>
         </div>
       </div>
-            <div className="">
-            </div>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Enterprise bot"
+        />
+        <CardContent>
+          <Typography gutterBottom component="h4">
+            Title required
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Description
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <div className="suggestion-controls">
+      <i class="material-icons intent-list-icons">add</i>
+      <small class="add-suggestion">Add Button</small>
+      </div>
+      </CardActions>
+    </Card>
     </div> 
   ) 
 
--- src/features/dashboard/intent-details/picture/picture.jsx
+++ src/features/dashboard/intent-details/picture/picture.jsx
@@ -1,7 +1,23 @@
 import React, { memo } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import './picture.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(1),
+        width: theme.spacing(16),
+        height: theme.spacing(16),
+      },
+    },
+}));
 
 
 export const Picture = memo(() => {
+    const classes = useStyles();
 
   return (
       <div className="suggestion-block">
@@ -13,8 +29,19 @@ export const Picture = memo(() => {
           <i class="material-icons delete-outline-icon">delete_outline</i>
         </div>
       </div>
-            <div className="">
-            </div>
+      <div className={classes.root}>
+      <Paper>
+      <div className="suggestion-controls picture">
+      <i class="material-icons intent-list-icons add-photo">add_a_photo</i>
+      <small class="add-suggestion upload-text">Upload image</small>
+      </div>
+      </Paper>
+      <Paper>
+      <div className="suggestion-controls picture">
+      <i class="material-icons intent-list-icons addphoto">add</i>
+      </div>
+      </Paper>
+    </div>
     </div> 
   ) 
 
